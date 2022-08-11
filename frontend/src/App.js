282cb9d8fb7fec3973506ca82982ba2890b528d6
mengyuan2208 <meng.yuan.2208@gmail.com>
2022-08-11 16:27:51 +0800

    Add a Navigation Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,20 @@ import {
 } from "react-router-dom";
 
 import Users from "./user/pages/Users";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
